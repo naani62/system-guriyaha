d82fd58cc2d76a47b2f594b69c69e9d59bebe7d5
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-26 23:00:22 +0100

    Update app.py

--- Real-Estate-Analysis-and-Prediction-master/app.py
+++ Real-Estate-Analysis-and-Prediction-master/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import seaborn 
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pickle as pk
